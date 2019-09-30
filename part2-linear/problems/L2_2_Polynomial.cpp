a93a22ebd5261d9c7412e8213e19727b0f8a159a
AshanJiang <ashanjiang@foxmail.com>
2019-09-30 17:09:25 +0800

    多项式乘加完成

--- part2-linear/problems/L2_2_Polynomial.cpp
+++ part2-linear/problems/L2_2_Polynomial.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 
 typedef struct PolyNode *Polynomial;
-struct PolyNode {
+struct PolyNode
+{
 	int coef;
 	int expon;
 	Polynomial link;
 };
 
 Polynomial ReadPoly();
-void PrintPoly();
+void PrintPoly(Polynomial P);
 void Attach(int c, int e, Polynomial &pRear);
 Polynomial Add(Polynomial P1, Polynomial P2);
 Polynomial Mult(Polynomial P1, Polynomial P2);
@@ -19,9 +20,9 @@ int main()
 	P1 = ReadPoly();
 	P2 = ReadPoly();
 	PP = Mult(P1, P2);
-	//PtintPoly(PP);
+	PrintPoly(PP);
 	PS = Add(P1, P2);
-	//PrintPoly(PS);
+	PrintPoly(PS);
 	return 0;
 }
 
@@ -33,19 +34,35 @@ Polynomial ReadPoly()
 	P = new PolyNode;
 	P->link = NULL;
 	Rear = P;
-	while (N--) {
+	while (N--)
+	{
 		cin >> c >> e;
 		Attach(c, e, Rear);
 	}
-	t = P; P = P->link; delete t;
+	t = P;
+	P = P->link;
+	delete t;
 	return P;
 }
 
-void PrintPoly()
+void PrintPoly(Polynomial P)
 {
+	int flag = 0; //控制空格，保证最后没有空格
+	if (!P)
+		cout << "0 0";
+	while (P)
+	{
+		if (!flag)
+			flag = 1;
+		else
+			cout << " ";
+		cout << P->coef << " " << P->expon;
+		P = P->link;
+	}
+	cout << endl;
 }
 
-void Attach(int c, int e, Polynomial & pRear)
+void Attach(int c, int e, Polynomial &pRear)
 {
 	Polynomial P;
 
@@ -61,34 +78,45 @@ Polynomial Add(Polynomial P1, Polynomial P2)
 {
 	Polynomial t1, t2, P, Rear, t;
 	int sum;
-	t1 = P1; t2 = P2;
-	P = new PolyNode; P->link = NULL;
+	t1 = P1;
+	t2 = P2;
+	P = new PolyNode;
+	P->link = NULL;
 	Rear = P;
-	while (t1&&t2)
+	while (t1 && t2)
 	{
-		if (t1->expon == t2->expon) {
+		if (t1->expon == t2->expon)
+		{
 			sum = t1->coef + t2->coef;
-			if (sum) Attach(sum, t1->expon, Rear);
-			t1 = t1->link; t2 = t2->link;
+			if (sum)
+				Attach(sum, t1->expon, Rear);
+			t1 = t1->link;
+			t2 = t2->link;
 		}
-		else if (t1->expon > t2->expon) {
+		else if (t1->expon > t2->expon)
+		{
 			Attach(t1->coef, t1->expon, Rear);
 			t1 = t1->link;
 		}
-		else {
+		else
+		{
 			Attach(t2->coef, t2->expon, Rear);
 			t2 = t2->link;
 		}
 	}
-	while (t1) {
+	while (t1)
+	{
 		Attach(t1->coef, t1->expon, Rear);
 		t1 = t1->link;
 	}
-	while (t2) {
+	while (t2)
+	{
 		Attach(t2->coef, t2->expon, Rear);
 		t2 = t2->link;
 	}
-	t = P; P = P->link; delete t;
+	t = P;
+	P = P->link;
+	delete t;
 	return P;
 }
 
@@ -96,39 +124,49 @@ Polynomial Mult(Polynomial P1, Polynomial P2)
 {
 	Polynomial P, Rear, t1, t2, t;
 	int c, e;
-	if (!P1 || !P2) return NULL;
+	if (!P1 || !P2)
+		return NULL;
 
-	t1 = P1; t2 = P2;
+	t1 = P1;
+	t2 = P2;
 	P = new PolyNode;
 	Rear = P;
-	while (t2) {
+	while (t2)
+	{
 		//1式第1项乘2式，得到最初式
-		Attach(t1->coef*t2->coef, t1->expon + t2->expon, Rear);
+		Attach(t1->coef * t2->coef, t1->expon + t2->expon, Rear);
 		t2 = t2->link;
 	}
 	t1 = t1->link;
 	// 其余项分别乘2式，并插入
-	while (t1) {
-		t2 = P2; Rear = P; //每轮循环要重置
-		while (t2) {
-			c = t1->coef*t2->coef;
+	while (t1)
+	{
+		t2 = P2;
+		Rear = P; //每轮循环要重置
+		while (t2)
+		{
+			c = t1->coef * t2->coef;
 			e = t1->expon + t2->expon;
 			//在有序多项式中插入新项
 			//找插入位置
-			while (Rear->link&&Rear->link->expon > e)
+			while (Rear->link && Rear->link->expon > e)
 				Rear = Rear->link;
-			if (Rear->link && Rear->link->expon == e) {
+			if (Rear->link && Rear->link->expon == e)
+			{
 				if (Rear->link->coef + c) //插入项
 					Rear->link->coef += c;
-				else { //抵消，删除这一项
+				else
+				{ //抵消，删除这一项
 					t = Rear->link;
 					Rear->link = t->link;
 					delete t;
 				}
 			}
-			else {
+			else
+			{
 				t = new PolyNode;
-				t->coef = c; t->expon = e;
+				t->coef = c;
+				t->expon = e;
 				t->link = Rear->link;
 				Rear->link = t;
 				Rear = Rear->link;
@@ -137,6 +175,8 @@ Polynomial Mult(Polynomial P1, Polynomial P2)
 		}
 		t1 = t1->link;
 	}
-	t2 = P; P = P->link; delete t2;
+	t2 = P;
+	P = P->link;
+	delete t2;
 	return P;
 }
