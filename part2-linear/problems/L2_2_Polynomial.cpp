fa71a6e1df3df1eeeafbd951d50b64cbdbef911a
AshanJiang <ashanjiang@foxmail.com>
2019-09-30 17:02:36 +0800

    多项式乘法

--- part2-linear/problems/L2_2_Polynomial.cpp
+++ part2-linear/problems/L2_2_Polynomial.cpp
@@ -12,12 +12,13 @@ Polynomial ReadPoly();
 void PrintPoly();
 void Attach(int c, int e, Polynomial &pRear);
 Polynomial Add(Polynomial P1, Polynomial P2);
+Polynomial Mult(Polynomial P1, Polynomial P2);
 int main()
 {
 	Polynomial P1, P2, PP, PS;
 	P1 = ReadPoly();
 	P2 = ReadPoly();
-	//PP = Mult(P1, P2);
+	PP = Mult(P1, P2);
 	//PtintPoly(PP);
 	PS = Add(P1, P2);
 	//PrintPoly(PS);
@@ -88,5 +89,54 @@ Polynomial Add(Polynomial P1, Polynomial P2)
 		t2 = t2->link;
 	}
 	t = P; P = P->link; delete t;
+	return P;
+}
+
+Polynomial Mult(Polynomial P1, Polynomial P2)
+{
+	Polynomial P, Rear, t1, t2, t;
+	int c, e;
+	if (!P1 || !P2) return NULL;
+
+	t1 = P1; t2 = P2;
+	P = new PolyNode;
+	Rear = P;
+	while (t2) {
+		//1式第1项乘2式，得到最初式
+		Attach(t1->coef*t2->coef, t1->expon + t2->expon, Rear);
+		t2 = t2->link;
+	}
+	t1 = t1->link;
+	// 其余项分别乘2式，并插入
+	while (t1) {
+		t2 = P2; Rear = P; //每轮循环要重置
+		while (t2) {
+			c = t1->coef*t2->coef;
+			e = t1->expon + t2->expon;
+			//在有序多项式中插入新项
+			//找插入位置
+			while (Rear->link&&Rear->link->expon > e)
+				Rear = Rear->link;
+			if (Rear->link && Rear->link->expon == e) {
+				if (Rear->link->coef + c) //插入项
+					Rear->link->coef += c;
+				else { //抵消，删除这一项
+					t = Rear->link;
+					Rear->link = t->link;
+					delete t;
+				}
+			}
+			else {
+				t = new PolyNode;
+				t->coef = c; t->expon = e;
+				t->link = Rear->link;
+				Rear->link = t;
+				Rear = Rear->link;
+			}
+			t2 = t2->link;
+		}
+		t1 = t1->link;
+	}
+	t2 = P; P = P->link; delete t2;
 	return P;
 }
