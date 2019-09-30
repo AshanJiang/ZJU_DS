72a4381831106a875015ee0c7b7ccfe5ca5d63a0
AshanJiang <ashanjiang@foxmail.com>
2019-09-30 16:13:07 +0800

    多项式加法

--- part2-linear/problems/L2_2_Polynomial.cpp
+++ part2-linear/problems/L2_2_Polynomial.cpp
@@ -11,15 +11,15 @@ struct PolyNode {
 Polynomial ReadPoly();
 void PrintPoly();
 void Attach(int c, int e, Polynomial &pRear);
+Polynomial Add(Polynomial P1, Polynomial P2);
 int main()
 {
 	Polynomial P1, P2, PP, PS;
 	P1 = ReadPoly();
 	P2 = ReadPoly();
-
 	//PP = Mult(P1, P2);
 	//PtintPoly(PP);
-	//PS = Add(P1, P2);
+	PS = Add(P1, P2);
 	//PrintPoly(PS);
 	return 0;
 }
@@ -54,4 +54,37 @@ void Attach(int c, int e, Polynomial & pRear)
 	P->link = NULL;
 	pRear->link = P;
 	pRear = P;
+}
+
+Polynomial Add(Polynomial P1, Polynomial P2)
+{
+	Polynomial t1, t2, P, Rear, t;
+	t1 = P1; t2 = P2;
+	P = new PolyNode; P->link = NULL;
+	Rear = P;
+	while (t1&&t2)
+	{
+		if (t1->expon == t2->expon) {
+			Attach(t1->coef + t2->coef, t1->expon, Rear);
+			t1 = t1->link; t2 = t2->link;
+		}
+		else if (t1->expon > t2->expon) {
+			Attach(t1->coef, t1->expon, Rear);
+			t1 = t1->link;
+		}
+		else {
+			Attach(t2->coef, t2->expon, Rear);
+			t2 = t2->link;
+		}
+	}
+	while (t1) {
+		Attach(t1->coef, t1->expon, Rear);
+		t1 = t1->link;
+	}
+	while (t2) {
+		Attach(t2->coef, t2->expon, Rear);
+		t2 = t2->link;
+	}
+	t = P; P = P->link; delete t;
+	return P;
 }
