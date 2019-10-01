ccf80acc5e54bead5d21d8ae256be4a2223cee9c
AshanJiang <ashanjiang@foxmail.com>
2019-10-01 15:00:58 +0800

    输入并构造链表

---
+++ part2-linear/problems/L2_3_ReverseLinked.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+typedef struct LinkedNode *LinkedList;
+struct LinkedNode {
+	int address;
+	int data;
+	int nextAddress;
+	LinkedList next;
+};
+struct find_address {
+	int address;
+	find_address(int address):address(address) {}
+	bool operator () (const LinkedList& n) const {
+		return n->address == address;
+	}
+};
+int formatAddress(string address);
+void printList(LinkedList list);
+int main() {
+	//1.读数据
+	int start, total, reverse, data;
+	string tmp;
+	cin >> tmp; start = formatAddress(tmp);
+	cin >> total; cin >> reverse;
+	vector<LinkedList> arr;
+	for (int i = 0; i < total; i++)
+	{
+		LinkedList node = new LinkedNode;
+		cin >> tmp;
+		node->address = formatAddress(tmp);
+		cin >> data;
+		node->data = data;
+		cin >> tmp;
+		node->nextAddress = formatAddress(tmp);
+		arr.push_back(node);
+	}
+	//2.构造链表
+	vector<LinkedList>::iterator it;
+	it = find_if(arr.begin(), arr.end(), find_address(start));
+	LinkedList list = (*it); //结果链表头
+	LinkedList cur = list; //表尾指针
+	while (true)
+	{
+		int nextAddress = cur->nextAddress;
+		if (nextAddress == -1)
+			break;
+		it = find_if(arr.begin(), arr.end(), find_address(nextAddress));
+		cur->next = *it;
+		cur = cur->next;
+	}
+	printList(list);
+	return 0;
+}
+
+int formatAddress(string address) {
+	return stoi(address.erase(0, min(address.find_first_not_of('0'), address.size() - 1)));
+}
+
+void printList(LinkedList list) {
+	while (list) {
+		cout << setw(5) << setfill('0') << list->address;
+		cout << ' ' << list->data << ' ';
+		if (list->nextAddress == -1)
+			cout << list->nextAddress;
+		else
+			cout << setw(5) << setfill('0') << list->nextAddress;
+		cout << endl;
+		list = list->next;
+	}
+}
