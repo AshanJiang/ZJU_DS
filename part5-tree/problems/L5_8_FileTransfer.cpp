6c6c028101c876041871db5295d2c2bb27868619
AshanJiang <ashanjiang@foxmail.com>
2019-10-19 10:14:33 +0800

    并查集

---
+++ part5-tree/problems/L5_8_FileTransfer.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+constexpr auto MaxSize = 10010;
+typedef int ElementType;
+typedef int SetName;
+typedef ElementType SetType[MaxSize];
+SetName Find(SetType S, ElementType X);
+void Union(SetType S, SetName Root1, SetName Root2);
+void Initialization(SetType &S, ElementType n);
+void Input_connection(SetType S);
+void Check_connection(SetType S);
+void Check_network(SetType S, ElementType n);
+
+int main()
+{
+	SetType S;
+	int n;
+	char in;
+	cin >> n;
+	Initialization(S, n);
+	do {
+		cin >> in;
+		switch (in)
+		{
+		case 'I': Input_connection(S); break;
+		case 'C': Check_connection(S); break;
+		case 'S': Check_network(S, n); break;
+		}
+	} while (in != 'S');
+	return 0;
+}
+
+SetName Find(SetType S, ElementType X) 
+{
+	//数组下标是数据，数组内容是父节点
+	for (; S[X] >= 0; X = S[X]);
+	return X;
+}
+
+void Union(SetType S, SetName Root1, SetName Root2)
+{
+	//假设Root2和Root1是不同集合根节点
+	S[Root2] = Root1; //把Root2连接向Root1
+}
+
+void Initialization(SetType & S, ElementType n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		S[i] = -1;
+}
+
+void Input_connection(SetType S)
+{
+	ElementType u, v;
+	SetName Root1, Root2;
+	cin >> u >> v;
+	//判断是否互连，若不，连接起来
+	Root1 = Find(S, u - 1);
+	Root2 = Find(S, v - 1);
+	if (Root1 != Root2) {
+		Union(S, Root1, Root2);
+	}
+}
+
+void Check_connection(SetType S)
+{
+	ElementType u, v;
+	SetName Root1, Root2;
+	cin >> u >> v;
+	Root1 = Find(S, u - 1);
+	Root2 = Find(S, v - 1);
+	if (Root1 == Root2)
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+}
+
+void Check_network(SetType S, ElementType n)
+{
+	int i, counter = 0;
+	for (i = 0; i < n; i++)
+		if (S[i] < 0) counter++;
+	if (counter == 1)
+		cout << "The network is connected." << endl;
+	else
+		cout << "There are " << counter << " components." << endl;
+}
