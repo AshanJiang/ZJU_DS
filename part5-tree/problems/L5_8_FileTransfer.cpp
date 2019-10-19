22148335d63f4270a65a5985ba326e700955fe5e
AshanJiang <ashanjiang@foxmail.com>
2019-10-19 10:26:51 +0800

    按规模归并

--- part5-tree/problems/L5_8_FileTransfer.cpp
+++ part5-tree/problems/L5_8_FileTransfer.cpp
@@ -7,6 +7,7 @@ typedef int SetName;
 typedef ElementType SetType[MaxSize];
 SetName Find(SetType S, ElementType X);
 void Union(SetType S, SetName Root1, SetName Root2);
+void Union_scale(SetType S, SetName Root1, SetName Root2);
 void Initialization(SetType &S, ElementType n);
 void Input_connection(SetType S);
 void Check_connection(SetType S);
@@ -40,13 +41,26 @@ SetName Find(SetType S, ElementType X)
 
 void Union(SetType S, SetName Root1, SetName Root2)
 {
+	//按高度归并
 	//假设Root2和Root1是不同集合根节点
 	if (S[Root2] < S[Root1])
 		S[Root1] = Root2;
 	else{
-		//按高度归并
 		if (S[Root2] == S[Root1]) S[Root1]--;
 		S[Root2] = Root1; //把Root2连接向Root1
+	}
+}
+
+void Union_scale(SetType S, SetName Root1, SetName Root2)
+{
+	//按规模归并
+	if (S[Root2] < S[Root1]) {
+		S[Root2] += S[Root1];
+		S[Root1] = Root2;
+	}
+	else {
+		S[Root1] += S[Root2];
+		S[Root2] = Root1;
 	}
 }
 
@@ -66,7 +80,7 @@ void Input_connection(SetType S)
 	Root1 = Find(S, u - 1);
 	Root2 = Find(S, v - 1);
 	if (Root1 != Root2) {
-		Union(S, Root1, Root2);
+		Union_scale(S, Root1, Root2);
 	}
 }
 
