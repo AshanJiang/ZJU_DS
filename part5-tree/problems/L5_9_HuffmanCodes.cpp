b2a64e72714a502f329da7531c5886d228fbcef3
AshanJiang <ashanjiang@foxmail.com>
2019-10-29 21:05:44 +0800

    构造哈夫曼树

--- part5-tree/problems/L5_9_HuffmanCodes.cpp
+++ part5-tree/problems/L5_9_HuffmanCodes.cpp
@@ -16,6 +16,8 @@ struct HNode {
 };
 MinHeap initMinHeap();
 void insertHeap(MinHeap H, HuffmanTree T);
+HuffmanTree deleteMin(MinHeap H);
+HuffmanTree inintHuffman(MinHeap H);
 int main() {
 	//N字符数量，f字符频率数组
 	int N, f[MAX_N], i, j;
@@ -38,6 +40,7 @@ int main() {
 		insertHeap(minHeap, t);
 	}
 	//2.2构造哈夫曼树
+	HuffmanTree hTree = inintHuffman(minHeap);
 	//3.计算最小码长
 	//4.读入待检测编码
 	//5.比较码长
@@ -59,7 +62,39 @@ MinHeap initMinHeap()
 void insertHeap(MinHeap H, HuffmanTree T) {
 	int i;
 	i = ++H->size;
-	for (; H->data[i / 2]->weight > T->weight; i /= 2)
+	for (; H->data[i / 2]->weight > T->weight; i /= 2) //如果插入小值，往上调整
 		H->data[i] = H->data[i / 2];
 	H->data[i] = T;
+}
+
+HuffmanTree deleteMin(MinHeap H) {
+	HuffmanTree minItem = H->data[1];
+	HuffmanTree X = H->data[H->size--]; //拿最后一个元素作为替换元素
+	int parent, child;
+	for (parent = 1; parent * 2 <= H->size; parent = child) {
+		child = parent * 2;
+		if (child != H->size&&H->data[child]->weight > H->data[child + 1]->weight)
+			child++; //child指向左右孩子较小值
+		if (X->weight <= H->data[child]->weight)
+			break;
+		else
+			H->data[parent] = H->data[child];
+	}
+	H->data[parent] = X;
+	return minItem;
+}
+
+HuffmanTree inintHuffman(MinHeap H) {
+	int i, n; HuffmanTree T;
+	n = H->size;
+	//循环堆大小(size-1)次
+	for (i = 1; i < n; i++) {
+		T = new TreeNode;
+		T->left = deleteMin(H);
+		T->right = deleteMin(H);
+		T->weight = T->left->weight + T->right->weight;
+		insertHeap(H, T);
+	}
+	T = deleteMin(H);
+	return T;
 }
